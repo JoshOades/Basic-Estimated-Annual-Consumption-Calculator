efde594d5b5bb36e05fab8650224a134b8f82b41
Oadzie <46132224+Oadzie@users.noreply.github.com>
2019-02-04 21:05:59 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -24,9 +24,13 @@ document.querySelector(".calculate-button").addEventListener("click", function()
     oneDay = 24*60*60*1000; // hours*minutes*seconds*milliseconds
     startDate = new Date(document.querySelector(".date-start").value);
     endDate = new Date(document.querySelector(".date-end").value);
+    totalConsumption = document.querySelector("#usage").value;
+
+    if(endDate > startDate && totalConsumption !== "") {
+
     daysCovered = Math.round(Math.abs((startDate.getTime() - endDate.getTime())/(oneDay)));
 
-    totalConsumption = document.querySelector("#usage").value;
+
 
     dailyConsumption = totalConsumption / daysCovered;
 
@@ -36,6 +40,10 @@ document.querySelector(".calculate-button").addEventListener("click", function()
     document.querySelector("#total-days").textContent = daysCovered;
     document.querySelector("#daily-consumption").textContent = Math.round(dailyConsumption);
     document.querySelector("#eac-consumption").textContent = Math.round(annualConsumption);
+
+    } else {
+        alert("Please ensure end date is greater than start date and that you have filled in consumption.");
+    }
 });
 
 
